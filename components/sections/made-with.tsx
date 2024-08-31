593f2a2c6bdbc73431d716e9dfa8d0c7ba6d93e7
wearekite <hello@kite.studio>
2024-08-31 02:12:43 +0200

    Update made-with.tsx

--- components/sections/made-with.tsx
+++ components/sections/made-with.tsx
@@ -3,10 +3,10 @@ import Link from "next/link";
 
 export function MadeWithSection() {
     return (
-        <section className="mx-auto max-w-md w-full p-4">
-        <Link href="https://t.me/gmonlinkbot?start" className="px-6 py-4 shadow rounded-xl flex flex-row items-center justify-center text-center gap-4 border transition-all duration-150 ease-in-out bg-background hover:bg-muted">
-                <Image src="/assets/gmon-purple.png" alt="gmon.link logo" width={64} height={64} className="rounded-lg border outline outline-foreground/30 outline-1 shadow size-8" />
-                <p className="text-base text-muted-foreground">Create your own gmon.link - it&apos;s free!</p>
+        <section className="md:fixed md:bottom-0 md:right-0 md:max-w-sm max-w-md md:w-auto w-full mt-auto md:mt-0 mx-auto md:mx-0 p-4">
+        <Link href="https://t.me/gmonlinkbot?start" className="px-4 py-3 shadow rounded-lg flex flex-row items-center justify-center text-center gap-2 border transition-all duration-150 ease-in-out bg-background hover:bg-muted">
+                <Image src="/assets/gmon-purple.png" alt="gmon.link logo" width={64} height={64} className="rounded-lg border outline outline-foreground/30 outline-1 shadow size-6" />
+                <p className="text-sm text-muted-foreground">Create your own gmon.link - it&apos;s free!</p>
         </Link>
         </section>
     )
