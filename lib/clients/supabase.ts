02dd5f9864c14356e51fa3a597d98b356149d64a
elevynto <dekanifisher@yahoo.co.uk>
2024-09-26 21:41:19 +0100

    set useSchema back to true

--- lib/clients/supabase.ts
+++ lib/clients/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const useSchema = false; // This is set to true as on the production version (hosted by Monad Pad) we are using a different schema for the database. When testing locally you can set this to false.
+const useSchema = true; // This is set to true as on the production version (hosted by Monad Pad) we are using a different schema for the database. When testing locally you can set this to false.
 
 export const supabase = createClient(process.env.NEXT_PUBLIC_SUPABASE_URL!, process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!, { db: { schema: useSchema ? "gmonlink" : undefined } });
 
