81afff11f26c9b070a97ad8e61a75ae04b87d6d9
elevynto <dekanifisher@yahoo.co.uk>
2024-10-09 11:22:22 +0100

    feat: implemented load more button with pagination on projects directory page

--- components/projects/projects-grid.tsx
+++ components/projects/projects-grid.tsx
@@ -6,7 +6,12 @@ import ProfileImage from "../ui/profile-image";
 import { IMAGE_BASE_URL } from "../../lib/utils";
 import { CheckCircle } from "lucide-react";
 
-export const ProjectsGrid = ({ projects }: { projects: VerifiedProject[] }) => {
+interface ProjectsGridProps {
+  projects: VerifiedProject[];
+  hasInited: boolean;
+}
+
+export const ProjectsGrid = ({ projects, hasInited }: ProjectsGridProps) => {
   return (
     <Suspense fallback={<ProjectsSkeletonGrid />}>
       {projects?.length ? (
@@ -34,7 +39,9 @@ export const ProjectsGrid = ({ projects }: { projects: VerifiedProject[] }) => {
           ))}
         </div>
       ) : (
-        <p className="text-lg sm:text-xl text-muted-foreground text-center">No projects found...</p>
+        <p className="text-lg sm:text-xl text-muted-foreground text-center">
+          {hasInited ? "No projects found..." : "Loading..."}
+        </p>
       )}
     </Suspense>
   );
--- lib/project/get-projects.ts
+++ lib/project/get-projects.ts
@@ -2,17 +2,31 @@
 import { supabase } from "../clients/supabase";
 import { VerifiedProject } from "./get-verified-projects";
 
-export async function getProjectsByQuery(query: string) {
-  const { data, error } = await supabase
+export async function getProjectsByQuery({
+  query,
+  limit = 1000,
+  range,
+}: {
+  query?: string;
+  limit?: number;
+  range?: { from: number; to: number };
+}) {
+  if (range && range?.from >= range?.to) {
+    throw new TypeError("Invalid range");
+  }
+
+  const { data, error, count } = await supabase
     .from("projects")
-    .select("title, slug, description, avatar_url")
+    .select("title, slug, description, avatar_url", { count: "exact" })
     .ilike("slug", !query ? "*" : `%${query.toLowerCase()}%`)
-    .order("created_at", { ascending: false });
+    .order("project_id", { ascending: true })
+    .limit(limit)
+    .range(range?.from ? range.from : 0, range?.to ? range.to : limit - 1);
 
   if (error) {
     console.error(error);
     throw new Error(error.message);
   }
 
-  return data as VerifiedProject[];
+  return { data: data as VerifiedProject[], count };
 }
